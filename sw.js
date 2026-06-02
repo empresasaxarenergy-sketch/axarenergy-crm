d2082a4493029f8d343d7a93a4f1e7f3e88a2ce1
empresasaxarenergy-sketch <empresasaxarenergy@gmail.com>
2026-06-02 08:39:55 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'axar-crm-v2';
+const FILES = ['./index.html','./manifest.json','./sw.js','./icon-192.svg','./icon-512.svg'];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES).catch(()=>{})));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(Promise.all([
+    clients.claim(),
+    caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+  ]));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if(cached) return cached;
+      return fetch(e.request).then(resp => {
+        if(!resp || resp.status!==200 || resp.type!=='basic') return resp;
+        caches.open(CACHE).then(c => c.put(e.request, resp.clone()));
+        return resp;
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
+});
